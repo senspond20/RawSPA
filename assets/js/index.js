537140a0bada0bfe4b4dda37099b4b3b17d6af21
senspond20 <senspond@gmail.com>
2021-09-29 18:21:18 +0900

    es6 to est5

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,54 @@
-import Dashboard from "./pages/Dashboard.js";
-import Posts from "./pages/Posts.js";
+// import Dashboard from "./pages/Dashboard.js";
+// import Posts from "./pages/Posts.js";
 //  import PostView from "./pages/PostView.js";
-import Settings from "./pages/Settings.js";
+// import Settings from "./pages/Settings.js";
 
-// function Settings(){
-    
-// }
+/* 추상 View 클래스 */
+var AbstractView = (function(){
+    function AbstractView(title, param){
+        console.log(title,param)
+        this.param = param;
+        this.title = title;
+        document.title = this.title
+    }
+    AbstractView.prototype.getHtml = function(){ return "dfsfd"}
+    AbstractView.prototype.didMount = function(){ return "dfsfd"}
+    return AbstractView;
+}());
 
-// Settings.prototype.getHtml = function(){
-//     return (`
-//             <h1>Welcome back, Dom</h1>
-//             <p>
-//                 Fugiat voluptate et nisi Lorem cillum anim sit do eiusmod occaecat irure do. Reprehenderit anim fugiat sint exercitation consequat. Sit anim laborum sit amet Lorem adipisicing ullamco duis. Anim in do magna ea pariatur et.
-//             </p>
-//             <p>
-//                 <a href="/posts" data-link>View recent posts</a>.
-//             </p>
-//     `)
-// }
+var Dashboard = (function(){
+    function Dashboard(title, param){
+        AbstractView.call(this,title,param)
+    }
+        Dashboard.prototype = Object.create(AbstractView.prototype)
+        Dashboard.prototype.getHtml = function(){
+            return (`
+                    <h1>안녕하세요</h1>
+            `)
+        }
+        Dashboard.prototype.didMount = function(){
+            document.querySelector('h1').addEventListener('click',()=>alert('hi'))
+        }
+    return Dashboard;
+}());
+
+
+
+function Settings(title){
+    document.title = title
+}
+
+Settings.prototype.getHtml = function(){
+    return (`
+            <h1>Welcome back, Dom</h1>
+            <p>
+                Fugiat voluptate et nisi Lorem cillum anim sit do eiusmod occaecat irure do. Reprehenderit anim fugiat sint exercitation consequat. Sit anim laborum sit amet Lorem adipisicing ullamco duis. Anim in do magna ea pariatur et.
+            </p>
+            <p>
+                <a href="/posts" data-link>View recent posts</a>.
+            </p>
+    `)
+}
 console.log(new Settings().getHtml())
 const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
 
@@ -37,10 +68,10 @@ const navigateTo = url => {
 
 const router = async () => {
     const routes = [
-        { path: "/", view: Dashboard },
-        { path: "/posts", view: Posts },
+        { path: "/", title : "홈" , view: Dashboard },
+        // { path: "/posts", view: Posts },
         // { path: "/posts/:id", view: PostView },
-        { path: "/settings", view: Settings }
+        { path: "/settings",  title : "Setting" , view: Settings }
     ];
 
     // Test each route for potential match
@@ -64,9 +95,14 @@ const router = async () => {
 
     console.log(match)
     console.log(getParams(match))
-    const view = new match.route.view(getParams(match));
+    const route = match.route;
+
+    const view = new route.view(route.title, getParams(match));
 
     document.querySelector("#app").innerHTML = await view.getHtml();
+
+    await view.didMount();
+
 };
 
 window.addEventListener("popstate", router);
